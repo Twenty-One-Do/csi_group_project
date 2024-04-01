06fde6925e74f186e009cbcf9ff9f878c5911e68
Pavel <blitzangreiffer@gmail.com>
2024-04-01 20:11:27 +0900

    2024.04.01 20:11
    UPDATE: DB 폴더 생성 및 run()

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import sqlite3
 
 app = Flask(__name__)
-connection = sqlite3.connect('database.db')
+connection = sqlite3.connect('database/database.db')
 
 cur = connection.cursor()
 
@@ -48,42 +48,54 @@ CREATE TABLE IF NOT EXISTS Comments (
 )
 ''')
 
+
 @app.route("/")
 def home():
     context = None
     return render_template("main.html", data=context)
+
 
 @app.route("/my_page")
 def my_page():
     context = None
     return render_template("my_page.html", data=context)
 
+
 @app.route("/til_list")
 def til_list():
     context = None
     return render_template("til_list.html", data=context)
+
 
 @app.route("/post/<post_id>")
 def post():
     context = None
     return render_template("post.html", data=context)
 
+
 @app.route("/write")
 def write():
     context = None
     return render_template("write.html", data=context)
+
 
 @app.route("/login")
 def login():
     context = None
     return render_template("login.html", data=context)
 
+
 @app.route("/register")
 def register():
     context = None
     return render_template("register.html", data=context)
 
+
 @app.route("/leaderboard")
 def leaderboard():
     context = None
     return render_template("leaderboard.html", data=context)
+
+
+if __name__ == "__main__":
+    app.run()
