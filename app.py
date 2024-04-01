4016db92f7c4be3ac3e883aceeb72c8551c810c5
wondo <dnjseh8962@gmail.com>
2024-04-01 17:01:05 +0900

    UPDATE: app.py DB 이니셜 코드 작성
    Database 이니셜 코드를 작성했습니다.
    처음 이니셜 후 주석처리하셔도 무방합니다.
    gitignore에 .db 파일을 무시하도록 설정했습니다.

--- app.py
+++ app.py
@@ -1,8 +1,50 @@
 from flask import Flask, render_template, request
 
-import random
 import requests
+import sqlite3
+
 app = Flask(__name__)
+connection = sqlite3.connect('database.db')
+
+cur = connection.cursor()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Members (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username CHAR(25) NOT NULL UNIQUE,
+    password CHAR(65) NOT NULL,
+    admin INTEGER NOT NULL DEFAULT 0, -- SQLite에서는 BOOLEAN 대신 INTEGER 사용
+    reg_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    last_acc_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    is_deleted INTEGER NOT NULL DEFAULT 0 -- SQLite에서는 BOOLEAN 대신 INTEGER 사용
+);
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title CHAR(25),
+    contents TEXT,
+    userid INTEGER,
+    reg_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    mod_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    consecutive_cnt INTEGER DEFAULT 1,
+    like_cnt INTEGER DEFAULT 1,
+    cnt INTEGER DEFAULT 1,
+    FOREIGN KEY(userid) REFERENCES another_table_name(another_table_id_column)
+)
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS Comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_id INTEGER,
+    user_id INTEGER,
+    contents TEXT,
+    reg_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    mod_date DATETIME DEFAULT CURRENT_TIMESTAMP
+)
+''')
 
 @app.route("/")
 def home():
